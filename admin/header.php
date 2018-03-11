f22d60b54fb5710afe3dd4e3ee9b02fb55a3e095
Sebahattin Altunay <saltunay@github.com>
2018-03-11 14:09:15 +0300

    first commit for code

---
+++ admin/header.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sebahattin ALTUNAY
+ * Date: 4.03.2018
+ * Time: 22:06
+ */
+?>
+
---
+++ admin/send-article.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sebahattin ALTUNAY
+ * Date: 6.03.2018
+ * Time: 23:03
+ */
+    include "config.php";
+?>
+
